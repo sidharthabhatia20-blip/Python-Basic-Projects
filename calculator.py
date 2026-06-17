3475990ea554005a4cc52d377ec27f3d5b6254ae
Sidhartha <sidharthabhatia20@gmail.com>
2026-06-17 12:31:51 +0530

    Update calculator.py
    Updated version and handling zero division problem

--- calculator.py
+++ calculator.py
@@ -1,22 +1,53 @@
+history = []
+
 r = 0
+
 while True:
+
     yn = input("Do you want to do calculation yes or no? ")
+
     if yn.lower() == "no":
         print("Calculation ended")
         break
+
+    a = float(input("Enter number: "))
+    op = input("Enter operator +, -, x, /: ")
+
+    if op == "+":
+
+        r += a
+
+    elif op == "-":
+
+        r -= a
+
+    elif op.lower() == "x":
+
+        if r == 0:
+            r = 1
+
+        r *= a
+
+    elif op == "/":
+
+        if a == 0:
+            print("Cannot divide by zero")
+            continue
+
+        r /= a
+
     else:
-        a = int(input("Enter number: "))
-        b = input("Enter operator +, -, x, (÷ or /): ")
-        if b == "÷" or b == "/":
-            r //= a
-        elif b == "+":
-            r += a
-        elif b.lower() == "x":
-            if r == 0:
-                r = 1
-            r *= a
-        elif b == "-":
-            r -= a
-        else:
-            print("Invalid arithmetic operator")
-    print(r)
+
+        print("Invalid arithmetic operator")
+        continue
+
+
+    history.append(f"{op} {a} = {r}")
+
+    print("Result:", r)
+
+
+print("\nCalculation History:")
+
+for i in history:
+    print(i)
